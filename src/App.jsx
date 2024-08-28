bf2334245285b8730e0129ed0f954574e5d12715
Muhammad Azlaan Zubair <mdazlaan1996@gmail.com>
2024-08-28 13:28:45 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="flex flex-col w-full h-[100vh] items-center justify-center">
+      <h1 className="text-red-500 text-2xl font-bold">Vite + React</h1>
+      <p className="text-lg text-blue-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </main>
+  );
+}
+
+export default App;
